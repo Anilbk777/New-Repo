42e0dca5d45e2bb940f85a7e7b6d3014675ca8eb
Anilbk777 <anilbk775@gmail.com>
2026-03-06 21:31:50 +0545

    school.py update

--- school.py
+++ school.py
@@ -1,60 +1,78 @@
 from __future__ import annotations
 from typing import List, Optional
 
-class Instructor:
-  def __init__(self,name:str):
-    self.name =  name
-    self.courses: List[Course] = []
 
-  def add_course(self, course: Course):
-    if course not in self.courses:
-      self.courses.append(course)
-      course.instructor = self
+class Instructor:
+    def __init__(self, name: str):
+        self.name = name
+        self.courses: List[Course] = []
+
+    def add_course(self, course: Course) -> None:
+        if course not in self.courses:
+            self.courses.append(course)
+            course.set_instructor(self)
+
+    def get_students(self) -> list[str]:
+      students = set()
+
+      for course in self.courses:
+          for student in course.students:
+              students.add(student.name)
+
+      return list(students)
 
 
 class Course:
-  def __init__(self,title:str):
-    self.title = title
-    self.instructor :Optional[Instructor] = None
-    self.students:List[Student] = []
+    def __init__(self, title: str):
+        self.title = title
+        self.instructor: Optional[Instructor] = None
+        self.students: set[Student] = set()
 
-  def enroll_student(self, student:Student):
+    def set_instructor(self, instructor: Instructor) -> None:
+        if self.instructor is not None and self.instructor != instructor:
+            raise ValueError("Course already has an instructor.")
+        self.instructor = instructor
 
-    if student.enrolled_courses is not None:
-      raise ValueError(
-          f"{student.name} already enrolled in {student.enrolled_course.title}"
-      )
-    self.students.append(student)
-    student.enrolled_courses = self
-
+    def enroll_student(self, student: Student) -> None:
+      if student not in self.students:
+          self.students.add(student)
+          student.courses.add(self)
 
 class Student:
-  def __init__(self,name:str) -> None:
-    self.name = name
-    self.enrolled_courses:Optional[Course] = None
+    def __init__(self, name: str):
+        self.name = name
+        self.courses: set[Course] = set()
 
-  def get_instructor_name(self) -> Optional[str]:
-    if self.enrolled_courses is not None:
-      return None
 
-    instructor = self.enrolled_courses.instructor
-    return instructor.name if instructor else None
+    def get_instructor_names(self) -> list[str]:
+      return [
+          course.instructor.name
+          for course in self.courses
+          if course.instructor is not None
+      ]
 
 
 if __name__ == "__main__":
 
     instructor = Instructor("Dr. Smith")
+    instructor2 = Instructor("Dr. Ram")
 
     course = Course("Python OOP")
+    course2 = Course("DSA")
 
     instructor.add_course(course)
+    instructor2.add_course(course2)
 
     student1 = Student("Alice")
     student2 = Student("Bob")
+    student3 = Student("john")
+
 
     course.enroll_student(student1)
     course.enroll_student(student2)
+    course2.enroll_student(student3)
 
     print(student1.get_instructor_name())
     print(student2.get_instructor_name())
-
+    print(student3.get_instructor_name())
+    instructor.get_student()
