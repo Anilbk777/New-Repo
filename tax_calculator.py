d163584cb1625cb4707ca4f161121ba75e96c1e0
Anilbk777 <anilbk775@gmail.com>
2026-03-10 10:15:03 +0545

    US tax calculator

--- tax_calculator.py
+++ tax_calculator.py
@@ -5,3 +5,8 @@ class TaxCalculator(ABC):
     @abstractmethod
     def calculate_tax(self, amount: float) -> float:
         pass
+
+
+class USTaxCalculator(TaxCalculator):
+    def calculate_tax(self, amount: float) -> float:
+        return 0.10 * amount
