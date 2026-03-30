b6ed47a9e241b6c4dda7f9fc0979489473bffe81
Anilbk777 <anilbk775@gmail.com>
2026-03-30 09:50:22 +0545

    add_text added

--- document_editor.py
+++ document_editor.py
@@ -1,4 +1,8 @@
 class DocumentEditor:
-  def __init__(self):
-    self.render_document:str = None
-    self.elements :list[str] = []
+    def __init__(self):
+        self.render_document:str = None
+        self.elements :list[str] = []
+    
+    def add_text(self, text:str):
+        self.elements.append(text)
+
