8d4ad9cd62ab1968a8956e8f920eecb8a8629eca
Anilbk777 <anilbk775@gmail.com>
2026-03-30 09:51:54 +0545

    add_image  added

--- document_editor.py
+++ document_editor.py
@@ -5,4 +5,17 @@ class DocumentEditor:
     
     def add_text(self, text:str):
         self.elements.append(text)
+    
+    def add_image(self, path:str):
+        self.elements.append(path)
+
+    def render_document(self):
+        if self.render_document is None:
+            for i in self.elements:
+                if i.endswith(".img"):
+                    self.render_document += "["+ i +"]" + "\n"
+                else:
+                    self.render_document += i + "\n"
+
+        return self.render_document
 
