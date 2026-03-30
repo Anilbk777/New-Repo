cd3f9c9d95521460b437b68134894741afb76fd0
Anilbk777 <anilbk775@gmail.com>
2026-03-30 09:53:04 +0545

    render_document function added

--- document_editor.py
+++ document_editor.py
@@ -2,10 +2,10 @@ class DocumentEditor:
     def __init__(self):
         self.render_document:str = None
         self.elements :list[str] = []
-    
+
     def add_text(self, text:str):
         self.elements.append(text)
-    
+
     def add_image(self, path:str):
         self.elements.append(path)
 
@@ -13,9 +13,8 @@ class DocumentEditor:
         if self.render_document is None:
             for i in self.elements:
                 if i.endswith(".img"):
-                    self.render_document += "["+ i +"]" + "\n"
+                    self.render_document += "[" + i + "]" + "\n"
                 else:
                     self.render_document += i + "\n"
 
         return self.render_document
-
