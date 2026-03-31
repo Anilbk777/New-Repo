c90f621d8a4b38749db60573204e24df9e6d1989
Anilbk777 <anilbk775@gmail.com>
2026-03-31 11:02:44 +0545

    document editor class added  and document element classes updated

--- document_editor_better.py
+++ document_editor_better.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class DocumentElement(ABC):
 
     @abstractmethod
@@ -9,47 +10,64 @@ class DocumentElement(ABC):
 
 class TextElement(DocumentElement):
 
+    def __init__(self, text: str):
+        self.text = text
+
     def render(self):
-        pass
+        return self.text
 
 
 class ImageElement(DocumentElement):
 
+    def __init__(self, path: str):
+        self.path = path
+
     def render(self):
-        pass
+        return "[Image: " + self.path + "]"
+
 
 class Persistance(ABC):
     @abstractmethod
     def save(self):
         pass
 
+
 class SaveToFile(Persistance):
-    def save(self,content:str):
+    def save(self, content: str):
         with open("document.txt", "w") as f:
             f.write(content)
-        
+
+
 class SaveToDB(Persistance):
     def save(self):
         print("save to my database.")
 
+
 class Document:
     def __init__(self):
-        self.elements:list[DocumentElement] = []
-    
+        self.elements: list[DocumentElement] = []
+
     def add_element(self, el: DocumentElement):
         self.elements.append(el)
 
     def get_element(self):
         return self.elements
-    
+
 
 class DocumentRender:
-    def __init__(self,doc: Document):
+    def __init__(self, doc: Document):
         self.doc = doc
-    
+
     def render(self):
-        elements= self.doc.get_element()
-        for  element in elements:
+        elements = self.doc.get_element()
+        for element in elements:
             print(element.render())
 
 
+class DocumentEditor:
+    def __init__(self, doc: Document, db: Persistance):
+        self.doc = doc
+        self.db = db
+
+    def add_text(self, text: str):
+        self.doc.add_element(TextElement())
